fb99082929d11aa77296cab4344bddf5554d8627
GaeunU <65946325+gaeunU@users.noreply.github.com>
2024-04-01 23:11:51 +0900

    DB 가져오기

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const server = app.listen(3000,() =>{
     console.log('Start Server: localhost:3000');
 });
 
+// DB 정보
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
 app.set('wiews',__dirname + '/views');
 
 // embeded javascript template
@@ -20,3 +29,22 @@ app.get('/', function (req, res) {
 app.get('/about', function (req, res) {
     res.render('about.html')
 })
+
+app.get('/db', function(req, res)){
+    pool.getConnection(function(err, connection) {
+        if (err) throw err; // not connected!
+        res.send(JSON.stringify(results));
+        console.log('results',results);
+        // Use the connection
+        // 모든 데이터를 가져와라 Test 테이블에서
+        connection.query('select * from Test', function (error, results, fields) {
+          // When done with the connection, release it.
+          connection.release();
+       
+          // Handle error after the release.
+          if (error) throw error;
+       
+          // Don't use the connection here, it has been returned to the pool.
+        });
+      });
+}
