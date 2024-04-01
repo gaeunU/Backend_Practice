4f01eb4fa4f6c8ca7e874ce43c9982f7ded8b657
GaeunU <65946325+gaeunU@users.noreply.github.com>
2024-04-01 22:41:34 +0900

    email 보내기

--- test.js
+++ test.js
@@ -1,6 +1,36 @@
 const moment = require("moment");
+const nodemailer = require("nodemailer");
 
 console.log("today", moment().format("YYYY-MM-DD"));
 console.log("today+1", moment().add(1,"day").format("YYYY-MM-DD"));
 console.log("week", moment().add(1,"week").format("YYYY-MM-DD"));
 console.log("month", moment().add(1,"month").format("YYYY-MM-DD"));
+
+const email={
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "af81acfda0f99c",
+      pass: ""
+    }
+};
+
+const send = async(option) => {
+    nodemailer.createTransport(email).sendMail(option, (error, info)=>{
+        if(error){
+            console.log(error);
+        }else{
+            console.log(info);
+            return info.response;
+        }
+    });
+};
+
+let emaildata={
+    from:'dbrkdmsyge12@naver.com',
+    to:'dbrkdmsyge12@naver.com',
+    subject: '테스트 메일',
+    text: '메일 연습이다.'
+}
+
+send(emaildata);
