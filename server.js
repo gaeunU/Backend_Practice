7e74c65781c4ddbc6c89ef0c3b85a8588f783f47
GaeunU <65946325+gaeunU@users.noreply.github.com>
2024-04-01 23:05:25 +0900

    html 과 node.js 로 서버 만들기

--- server.js
+++ server.js
@@ -5,11 +5,18 @@ const server = app.listen(3000,() =>{
     console.log('Start Server: localhost:3000');
 });
 
+app.set('wiews',__dirname + '/views');
+
+// embeded javascript template
+// html 안에서 자바스크립트를 같이 사용하도록 해주는 템플릿
+app.set('view engine','ejs'); 
+app.engine('html', require('ejs').renderFile);
+
 // 라우터
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.render('index.html')
 })
 
 app.get('/about', function (req, res) {
-    res.send('about page')
+    res.render('about.html')
 })
